f5dd94b0dee46c5462c113d37666acbf3558b68a
Raj Aththanayake <rajitha_kba@yahoo.com.au>
2013-11-28 23:17:13 +1100

    initial commit

---
+++ Container/AutoMoqContainer.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+using Microsoft.Practices.Unity;
+using Moq;
+
+namespace AutoMocking.UnityMoq
+{
+    public enum DefaultBehavior
+    {
+        Empty = 0,
+        AutoMock = 1,
+    }
+
+    /// <summary>
+    /// Define members for auto mocking functionality
+    /// with configured contains and the isolation object framework.
+    /// </summary>
+    public class AutoMockContainer : UnityContainer
+    {
+        private readonly DefaultBehavior _defaultBehavior;
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public AutoMockContainer()
+            : this(DefaultBehavior.AutoMock)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="defaultBehavior"></param>
+        public AutoMockContainer(DefaultBehavior defaultBehavior)
+        {
+            _defaultBehavior = defaultBehavior;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public DefaultBehavior DefaultBehavior {
+            get
+            {
+                return _defaultBehavior;
+            }
+        }
+
+    }
+}
